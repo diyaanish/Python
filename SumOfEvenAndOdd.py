39ea84d337fbb8a9003f89f01727083f444447c3
diyaanish <diya.anish.r@gmail.com>
2026-08-23 20:17:45 +0530

    Create SumOfEvenAndOdd.py

---
+++ SumOfEvenAndOdd.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+i = 2
+sum = 0
+while(i<=n):
+  sum = sum + i
+  i += 2
+print(f"Sum of even numbers:{sum}")
+
+i = 1
+sum = 0
+while(i<=n):
+  sum = sum + i
+  i += 2
+print(f"Sum of odd numbers:{sum}
